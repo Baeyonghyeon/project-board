b30fb93130b3b189db330a7a95ae6a2129d06870
kurt <qodydgus97@kakao.com>
2022-11-16 17:34:17 +0900

    #20 - 게시판 컨트롤러 구현
    게시판 기본 조회 화면을 위한 컨트롤러 구현하고 데이터 전달 완료

--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -1,15 +1,28 @@
 package com.fastcampus.projectboard.controller;
 
 import com.fastcampus.projectboard.config.SecurityConfig;
+import com.fastcampus.projectboard.dto.ArticleWithCommentsDto;
+import com.fastcampus.projectboard.dto.UserAccountDto;
+import com.fastcampus.projectboard.service.ArticleService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -20,6 +33,9 @@ class ArticleControllerTest {
 
     private final MockMvc mvc;
 
+    @MockBean
+    private ArticleService articleService;
+
     ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
@@ -27,24 +43,37 @@ class ArticleControllerTest {
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
     @Test
     public void givenNoting_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
-
+        //given
+        given(articleService.searchArticles(eq(null), eq(null), any(Pageable.class)))
+                .willReturn(Page.empty());
+        //when & then
         mvc.perform(get("/articels"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML)) // 같은 유형은 맞다고 해줌.
                 .andExpect(view().name("articles/index"))
                 .andExpect(model().attributeExists("articles"));
+
+        then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
     }
 
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNoting_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
 
-        mvc.perform(get("/articels/1"))
+        //given
+        Long articleId = 1L;
+        given(articleService.getArticle(articleId))
+                .willReturn(createArticleWithCommentDto());
+        //when & then
+
+        mvc.perform(get("/articels/" + articleId))
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
+
+        then(articleService).should().getArticle(articleId);
     }
 
     @Disabled("구현중")
@@ -67,5 +96,34 @@ class ArticleControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search-hashtag"));
+    }
+
+    private ArticleWithCommentsDto createArticleWithCommentDto(){
+        return ArticleWithCommentsDto.of(
+                1L,
+                createUserAccountDto(),
+                Set.of(),
+                "title",
+                "content",
+                "#hashtag",
+                LocalDateTime.now(),
+                "kurt",
+                LocalDateTime.now(),
+                "kurt"
+        );
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(1L,
+                "kurt",
+                "pw",
+                "kurt@kakao.com",
+                "kurt",
+                "memo",
+                LocalDateTime.now(),
+                "kurt",
+                LocalDateTime.now(),
+                "kurt"
+                );
     }
 }
